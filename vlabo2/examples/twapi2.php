22375b6394c051ace63b45ba7a43c37e3ac65dfb
Tokitaka Abe <vortex003@msn.com>
2018-08-26 04:22:02 +0900

    ver0.3

---
+++ vlabo2/examples/twapi2.php
@@ -0,0 +1,3 @@
+<body bgcolor="white">
+<?php $id=$_GET['id'];?>
+<a class="twitter-timeline" href="https://twitter.com/<?php echo $id;?>?ref_src=twsrc%5Etfw">Tweets by satoatu_jp</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
